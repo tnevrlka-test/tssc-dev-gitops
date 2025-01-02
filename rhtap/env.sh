7a8174ee44ce53abb665d0412c22b6229cbc7b90
Tomas Nevrlka <tnevrlka@redhat.com>
2025-01-02 17:10:57 +0100

    Testing in CI

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -53,4 +53,4 @@ export SBOMS_DIR=results/sboms
 export REKOR_HOST=
 export IGNORE_REKOR=true
 export TUF_MIRROR=
-# Update forced CI test čt 2. ledna 2025, 17:09:01 CET
+# Update forced CI test čt 2. ledna 2025, 17:10:25 CET
